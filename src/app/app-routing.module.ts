ad619ca4728712f766050f02d3d4f1ade82b173c
Mateo-11 <mateonaz11@gmail.com>
2020-04-01 12:26:33 +0800

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BlogApiService } from './blog-api.service';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { SingleComponent } from './blog/single/single.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+	{path: '', children: [
+		{path: '', component: HomeComponent},
+	    {path: '', component: HeaderComponent, outlet: 'header'},
+	    {path: 'blog', component: BlogComponent},
+	    {path: 'blog/:postname', component: SingleComponent},
+	    {path: 'contact', component: ContactComponent},
+	]},
+
+	{path: '**', redirectTo: ''}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
